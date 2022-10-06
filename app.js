f8a33b5496c74a2f29117ffad07c714fbd2598d2
OleksiiDeiko <alexei.deyko@gmail.com>
2022-10-06 22:20:26 +0200

    basic logic

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+//middleware
+
+app.use(express.json())
+
+
+
+
+//routes
+
+app.get('/hello', (req, res) => {
+    res.send('Task Manager') 
+})
+
+app.use('/api/tasks', tasks)
+
+const PORT = 3000
+
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URL)
+        app.listen(PORT, console.log(`server is listening on port ${PORT}...`))
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start()
---
+++ controllers/tasks.js
@@ -0,0 +1,32 @@
+const task = require('../models/task')
+const getAllTasks = (req, res) => {
+    res.send('get all tasks')
+}
+
+const createTask = async (req, res) => {
+    const newTask = await task.create(req.body)
+    res.status(201).json({newTask})
+}
+
+const getTask = (req, res) => {
+    res.json({ id: req.params.id })
+}
+
+const updateTask = (req, res) => {
+    res.send('update task')
+}
+
+const deleteTask = (req, res) => {
+    res.send('delete task')
+}
+
+
+
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
